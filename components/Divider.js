67773c8d9be5811a9011b1a3b587773acb89a6fd
Danilo Leal <daniloleal09@gmail.com>
2021-01-24 19:26:51 -0300

    divider colour change

--- components/Divider.js
+++ components/Divider.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Divider() {
   return (
     <div className="py-4">
-      <div className="h-0.5 bg-gray-100 dark:bg-blackish w-full"></div>
+      <div className="h-0.5 bg-gray-100 dark:bg-black w-full"></div>
     </div>
   );
 }
